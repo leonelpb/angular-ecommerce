2757ce1b1f559c7ec2589a6e6ff995d0069d9a64
Leonel <leonelperez.b23@gmail.com>
2022-10-14 09:09:25 -0300

    deploy

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,5 +1,5 @@
 import { environment } from "src/environments/environment";
 
-export const productUrl = environment.production ? 'https://api.shoppingcart.com': 'https://fakestoreapi.com/products' ;
-export const cartUrl= 'https://my-json-server.typicode.com/leonelpb/dbjson/cart';
-export const wishListUrl = 'https://my-json-server.typicode.com/leonelpb/dbjson/wishList';
+export const productUrl = 'https://fakestoreapi.com/products' ;
+export const cartUrl= 'https://my-json-server.typicode.com/leonelpb/angular-ecommerce/cart';
+export const wishListUrl = 'https://my-json-server.typicode.com/leonelpb/angular-ecommerce/wishList';
